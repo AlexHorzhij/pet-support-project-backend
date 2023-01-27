7a56645ee80432eca4467a67c62ba2c803148639
Kateryna Skoryna <k.skoryna@gmail.com>
2023-01-27 18:19:01 +0100

    Add models and DB

---
+++ models/users.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Phone number is required'],
+    },
+    city: {
+      type: String,
+      required: [true, 'City is required'],
+    },
+    avatarURL: {
+      type: String,
+      required: true,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
+  },
+  { versionKey: false, timestaps: true }
+);
+
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const app = require("./app");
-const mongoose = require("mongoose");
-const { DB_HOST, PORT = 3000 } = process.env;
-
+const app = require('./app');
+const mongoose = require('mongoose');
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connection successful"))
+  .then(() => console.log('Database connection successful'))
   .then(() =>
     app.listen(3000, () => {
-      console.log("Server running!!! Use our API on port: 3000");
+      console.log('Server running!!! Use our API on port: 3000');
     })
   )
-  .catch((error) => {
+  .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
