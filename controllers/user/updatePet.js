c5d38fe69a6cdf61f61f48404a454ad1b6d99f24
Kateryna Skoryna <k.skoryna@gmail.com>
2023-01-31 14:04:59 +0100

    Fix updatePets

--- controllers/user/updatePet.js
+++ controllers/user/updatePet.js
@@ -1,13 +1,12 @@
-const { Pet } = require("../../models");
+const { Pet } = require('../../models');
 const { NotFound } = require('http-errors');
 
-
 const updatePet = async (req, res, next) => {
-    const { petId } = req.params;
+  const petId = req.params.id;
   const result = await Pet.findByIdAndUpdate(petId, req.body);
 
   if (!result) {
-    throw new NotFound("Not found");
+    throw new NotFound('Not found');
   }
   res.json(result);
 };
