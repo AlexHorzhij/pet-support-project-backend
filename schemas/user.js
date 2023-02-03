ae961b621c6267b3eb0497a17c4ffb959fc8cae9
Elena Tatsenko <lena.harchenko24@gmail.com>
2023-02-03 18:06:28 +0200

    fix schemas user & pet

--- schemas/user.js
+++ schemas/user.js
@@ -1,19 +1,19 @@
 const Joi = require('joi');
 
 const user = {
-  name: Joi.string().required(),
-  email: Joi.string().required().email(),
-  password: Joi.string().required().alphanum().min(7).max(32),
+  name: Joi.string(),//.required(),
+  email: Joi.string()/*.required()*/.email(),
+  password: Joi.string()/*.required()*/.alphanum().min(7).max(32),
   avatarUrl: Joi.string().optional(),
   birthdate: Joi.string()
     .optional()
     .pattern(new RegExp('(0[1-9]|[12][0-9]|3[01]).(0[1-9]|1[012]).(19|20)')),
   phone: Joi.string()
-    .required()
+    //.required()
     .pattern(
       new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$')
     ),
-  city: Joi.string().required(),
+  city: Joi.string()//.required(),
 };
 
 const signupUserSchema = Joi.object({
