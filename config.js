ec2f534320a973c0506179cda6e8db3b9ec20fd0
Elena Tatsenko <lena.harchenko24@gmail.com>
2023-02-03 17:49:02 +0200

    add config from cloudinary

--- config.js
+++ config.js
@@ -3,8 +3,11 @@ dotenv.config();
 
 module.exports = {
   DB_HOST: process.env.DB_HOST,
-  PORT:process.env.PORT,
+  PORT: process.env.PORT,
   SECRET_KEY: process.env.SECRET_KEY,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  BASE_URL:process.env.BASE_URL
+  BASE_URL: process.env.BASE_URL,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
