618ef2fca8dc509f39bb833753df267c135fb976
Kateryna Skoryna <k.skoryna@gmail.com>
2023-02-05 21:01:01 +0100

    Fix routes and added avatar to notice

--- controllers/notice/addUserNotice.js
+++ controllers/notice/addUserNotice.js
@@ -3,8 +3,21 @@ const { Notice } = require('../../models');
 const addUserNotice = async (req, res) => {
   const userId = req.user._id;
   const date = Date.now();
+  const { file } = req;
+  if (!file) {
+    avatarUrl =
+      'http://res.cloudinary.com/djkbwd06u/image/upload/v1675352872/foewy92xelkruou5221u.jpg';
+  } else {
+    const fileFormat = file.mimetype.split('/')[1];
+    const { base64 } = formatParcer(fileFormat, file.buffer);
+
+    const imageDetails = await uploadToCloudinary(base64, fileFormat);
+
+    avatarUrl = imageDetails.url;
+  }
   const result = await Notice.create({
     ...req.body,
+    avatarUrl,
     create_at: date,
     owner: userId,
   });
