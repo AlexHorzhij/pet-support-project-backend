a61f43cd51173f2cb4eac42263519cc6c991cf3d
Elena Tatsenko <lena.harchenko24@gmail.com>
2023-02-06 17:11:03 +0200

    change verification leter, change defolt avatar to pet & user

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -20,7 +20,7 @@ const signup = async (req, res) => {
   const { file } = req;
   if (!file) {
     avatarUrl =
-      'http://res.cloudinary.com/djkbwd06u/image/upload/v1675352872/foewy92xelkruou5221u.jpg';
+      'http://res.cloudinary.com/djkbwd06u/image/upload/v1675694970/jshyicvhcfjtr9sb2fon.jpg';
   } else {
     const fileFormat = file.mimetype.split('/')[1];
     const { base64 } = formatParcer(fileFormat, file.buffer);
@@ -39,7 +39,12 @@ const signup = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: 'Verify your email',
-    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify email</a>`,
+    html: `<div style="font-style: medium; line-height: 1; letter-spacing: 0.04em; text-align: center; background-color: rgb(253, 247, 242); padding: 20px;">
+    <p style=" font-size: 48px; color: black; margin-bottom: 20px;">You have successfully registered in the <span style="line-height: 48px; font-weight: 700; ont-size: 50px;">pe<span style="color: orange">t</span>ly</span>.</p>
+    <p style=" font-size: 36px; color:orange; margin-bottom: 20px;">Follow the link for further use</p>
+    <button style="cursor: pointer; text-align: center; vertical-align: middle;line-height: 1.37;border-radius: 40px; margin-bottom: 20px; min-width: 64px;
+    padding: 5px 15px; border: 2px solid rgb(245, 146, 86);background-color: white;"><a style="text-decoration: none; font-size:36px; color:orange; line-height: 1.37;" target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">verification</a></button>
+    <p style="font-size: 36px; color: black;">If you did not register and you received the email by mistake, ignore it.</p></div>`,
   };
   await sendEmail(verifyEmail);
 
