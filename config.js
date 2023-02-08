e5a0d641ed1ffaf5e768a1d108457401cd7c5cc5
Elena Tatsenko <lena.harchenko24@gmail.com>
2023-02-09 00:11:55 +0200

    add email to env & config, add verifyEmail to helpers

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
   PORT: process.env.PORT,
   SECRET_KEY: process.env.SECRET_KEY,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  SENDGRID_EMAIL:process.env.SENDGRID_EMAIL,
   BASE_URL: process.env.BASE_URL,
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
--- helpers/index.js
+++ helpers/index.js
@@ -2,12 +2,14 @@ const handleSchemaValidationErrors = require('./handleSchemaValidationErrors');
 const ctrlWrapper = require('./ctrlWrapper');
 const sendEmail = require('./sendMail');
 const formatParcer = require('./formatParcer');
-const uploadToCloudinary = require('./uploadToCloudinary')
+const uploadToCloudinary = require('./uploadToCloudinary');
+const verifyEmail = require('./verifyEmail')
 
 module.exports = {
   handleSchemaValidationErrors,
   ctrlWrapper,
   sendEmail,
   formatParcer,
-  uploadToCloudinary
+  uploadToCloudinary,
+  verifyEmail
 };
